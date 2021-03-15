dccde30a920580d6670db244c13930ae9c2898c5
Nicole Simon <33nicole.simon@gmail.com>
2021-03-14 19:52:31 -0500

    Pressing Q to Quit

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,14 +42,16 @@ class AlienInvasion:
         """Respond to keypresses."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
     def _update_screen(self):
