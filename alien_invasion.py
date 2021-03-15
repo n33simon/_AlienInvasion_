85636657d69411994c957240f5870940edcc8c87
Nicole Simon <33nicole.simon@gmail.com>
2021-03-15 06:45:35 -0500

    Displaying the Level

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,6 +75,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             #Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -139,6 +140,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            #Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
