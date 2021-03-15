edd9e56efd8683079b726a69351a53742da44aee
Nicole Simon <33nicole.simon@gmail.com>
2021-03-14 20:15:43 -0500

    System Check Errors

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,7 +18,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-        
+
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -41,7 +41,7 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-            
+
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
         if event.key == pygame.K_RIGHT:
@@ -72,6 +72,7 @@ class AlienInvasion:
             bullet.draw_bullet()
 
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     # Make a game instance, and run the game.
